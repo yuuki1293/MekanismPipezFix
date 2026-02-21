5d1a5dffbd24a873f463e3818a5eb75356134d0a
yuuki1293 <yuuki1293@outlook.jp>
2026-02-22 00:27:13 +0900

    不要な処理を削除

--- src/main/java/com/github/yuuki1293/mekpipezfix/MekPipezFix.java
+++ src/main/java/com/github/yuuki1293/mekpipezfix/MekPipezFix.java
@@ -1,25 +1,15 @@
 package com.github.yuuki1293.mekpipezfix;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(MekPipezFix.MODID)
 public class MekPipezFix {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "mekpipezfix";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public MekPipezFix(FMLJavaModLoadingContext context) {
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        context.registerConfig(ModConfig.Type.SERVER, Config.SPEC);
     }
 }
