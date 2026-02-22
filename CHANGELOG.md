f223b2de57bc7ceb23d7921026fbe359c6682be1
yuuki1293 <yuuki1293@outlook.jp>
2026-02-22 15:36:53 +0900

    update version to v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,13 @@
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/)
 
+## [1.0.1]
+
+### Fixed
+
+- Not working with Turbine Vent.
+- Insert is not working.
+
 ## [1.0.0]
 
 ### Added
--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
         const val name = "Mekanism Pipez Fix"
         const val description = "Fix a bug where Pipez disconnects from Mekanism multiblocks."
         const val license = "MIT"
-        const val version = "1.0.0"
+        const val version = "1.0.1"
         const val group = "yuuki1293"
         const val author = "yuuki1293"
         const val repositoryUrl = "https://github.com/yuuki1293/MekanismPipezFix"
